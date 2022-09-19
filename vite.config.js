34726f196d221305e1ee55a3c19c8e7261204c1e
Rodrigo <rodriigosantos11@gmail.com>
2022-09-19 14:29:39 -0300

    .

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,6 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  root: resolve(__dirname),
   base: '/pokedex/',
   build: {
     outDir: "docs",
